5fe438e48814f02efaa9b82612c2025cf8469618
Erik-42 <mesen.erik@gmail.com>
2024-04-22 16:46:48 +0200

    Search tags

--- scripts/search/input_search.js
+++ scripts/search/input_search.js
@@ -1,29 +1,46 @@
 import { tags } from "../utils/tags.js";
+import { majAppareil } from "./majAppareil.js";
+import { majIngredients } from "./majIngredients.js";
+import { majUstensils } from "./majUstensiles.js";
 
-export function inputSearch(value, recipes) {
+export function inputSearch(value, recipes, appliances) {
   //sur deuxième branche interdit filter, find, map, foreach, findindex, includes, etc...
   let recipesFiltred = [...recipes];
 
   if (value.length >= 3 || tags) {
     // console.log("filtre by input", value);
     recipesFiltred = recipes.filter((recipe) =>
-      recipe.name.match(new RegExp(value, "i"))
-    );
-  } else if (value.length >= 3) {
-    recipesFiltred = recipes.filter((recipe) =>
-      recipe.appliance.match(new RegExp(value, "i"))
-    );
-  } else if (value.length >= 3) {
-    recipesFiltred = recipes.filter((recipe) =>
-      recipe.ustensils.match(new RegExp(value, "i"))
-    );
-  } else if (value.length >= 3) {
-    recipesFiltred = recipes.filter((recipe) =>
-      recipe.ingredients.match(new RegExp(value, "i"))
+      recipe.name.toLowerCase.match(new RegExp(value, "i"))
     );
   }
+  if (
+    recipe.description.toLowerCase().includes(e.target.value) ||
+    recipe.appliance.toLowerCase().includes(e.target.value) ||
+    recipe.ustensils.find(ingredient) => {
+      return ingredient.ingredient.toLowerCase().includes(e.target.value)
+    } ||
+    recipe.ingredients.find(ingredient) => {
+      return ingredient.ingredient.toLowerCase().includes(e.target.value)
+    } != undefined
+  ) {
+    recipesFiltred = recipes.filter((recipe) =>
+      recipe.description.toLowerCase.match(new RegExp(value, "i"))
+    );
+  }
+ 
+  
+  
+
   console.log(recipesFiltred);
   console.log(recipes);
+  console.log(tags);
 
+  if (recipesFiltred.length === 0) {
+    const noResult = document.getElementById("noResult");
+    noResult.textContent = `Aucune recette ne correspond à vos critères... vous pouvez chercher "tarte aux pommes", "poisson", etc.`;
+    noResult.classList.remove("hidden");
+  } else {
+    noResult.classList.add("hidden");
+  }
   return recipesFiltred;
 }
--- scripts/search/tag_search.js
+++ scripts/search/tag_search.js
@@ -1,5 +1,28 @@
-export function tagSearch(tag, recipes) {
-  const recipesFiltred = [...recipes];
+import { tags } from "../utils/tags.js";
+import { majAppareil } from "./majAppareil.js";
+import { majUstensils } from "./majUstensiles.js";
+import { majIngredients } from "./majIngredients.js";
 
-  return recipesFiltred;
+export function tagSearch(tag, recipes) {
+  let recipesFiltred = [...recipes];
+
+  recipes.forEach((tags) => {
+    if (tags === "appliance") {
+      recipesFiltred = recipes.filter((recipe) =>
+        recipe.appliance.includes(majAppareil)
+      );
+    }
+  });
+
+  let appliancesTags = { recipesFiltred, majAppareil, tags };
+  recipes.forEach((recipe) => {
+    if (
+      majAppareil.includes(recipe.appliance) &&
+      !tags.includes(recipe.appliance)
+    ) {
+      tags.push(recipe.appliance);
+    }
+    if (tags.length === 0) return majAppareil;
+  })(recipesFiltred);
+  return appliancesTags;
 }
